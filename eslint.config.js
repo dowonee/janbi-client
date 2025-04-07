1a580def0e1ca03da660a46cec0d86e574a9f5b1
dowonee <jjddwwyt@gmail.com>
2025-04-07 23:35:42 +0900

    Initial Commit

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+import js from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
+import prettier from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import unusedImports from "eslint-plugin-unused-imports";
+import babelParser from "@babel/eslint-parser";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      import: importPlugin,
+      prettier,
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      "no-console": "warn",
+      "no-unused-vars": "warn",
+      "unused-imports/no-unused-imports": "warn",
+      "import/no-extraneous-dependencies": "off",
+      "react/jsx-props-no-spreading": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/button-has-type": "off",
+      "prefer-arrow-callback": "off",
+      "prettier/prettier": "error",
+      "import/order": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
