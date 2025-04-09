a5398a140204d33075aff076f21e4b9d55a69d3c
dowonee <jjddwwyt@gmail.com>
2025-04-10 01:35:15 +0900

    feat: 등록된 URL이 없을 때 UI 구현(#3)

--- src/components/dashboard/UrlList.jsx
+++ src/components/dashboard/UrlList.jsx
@@ -68,44 +68,52 @@ export default function UrlList() {
             URL 추가
           </button>
         </div>
-        <div className="grid grid-cols-[1.2fr_2fr_1fr_1fr_1.5fr_1fr] bg-gray-100 px-4 py-2 text-xs font-semibold text-gray-600 border-y">
-          <div className="text-center">이름</div>
-          <div className="text-center">URL</div>
-          <div className="text-center">상태</div>
-          <div className="text-center">변경 횟수</div>
-          <div className="text-center">마지막 확인</div>
-          <div className="text-center">체크 주기</div>
-        </div>
-
-        {urls.map((urlInfo) => (
-          <div
-            key={urlInfo._id}
-            className="grid grid-cols-[1.2fr_2fr_1fr_1fr_1.5fr_1fr] px-4 py-3 text-sm text-gray-800 border-b urlInfos-center hover:bg-gray-50 transition-colors"
-          >
-            <div className="text-center">{urlInfo.name}</div>
-            <div className="text-center">{urlInfo.url}</div>
-            <div className="text-center">
-              <span
-                className={`inline-flex urlInfos-center px-2.5 py-0.5 rounded-full text-xs font-medium ${
-                  urlInfo.status === "정상"
-                    ? "bg-lime-100 text-slate-900"
-                    : urlInfo.status === "변경감지"
-                      ? "bg-blue-100 text-slate-900"
-                      : "bg-red-100 text-slate-900"
-                }`}
-              >
-                {urlInfo.status}
-              </span>
-            </div>
-            <div className="text-center">{urlInfo.changeCount}</div>
-            <div className="text-center">
-              {formatTime(urlInfo.lastChecked).slice(0, -3)}
-            </div>
-            <div className="text-center">
-              {formatInterval(urlInfo.checkInterval)}
-            </div>
+        {urls.length === 0 ? (
+          <div className="flex flex-col items-center justify-center py-20 text-gray-500 text-sm">
+            <p className="mb-3">모니터링할 URL이 없습니다.</p>
           </div>
-        ))}
+        ) : (
+          <>
+            <div className="grid grid-cols-[1.2fr_2fr_1fr_1fr_1.5fr_1fr] bg-gray-100 px-4 py-2 text-xs font-semibold text-gray-600 border-y">
+              <div className="text-center">이름</div>
+              <div className="text-center">URL</div>
+              <div className="text-center">상태</div>
+              <div className="text-center">변경 횟수</div>
+              <div className="text-center">마지막 확인</div>
+              <div className="text-center">체크 주기</div>
+            </div>
+
+            {urls.map((urlInfo) => (
+              <div
+                key={urlInfo._id}
+                className="grid grid-cols-[1.2fr_2fr_1fr_1fr_1.5fr_1fr] px-4 py-3 text-sm text-gray-800 border-b justify-center hover:bg-gray-50 transition-colors"
+              >
+                <div className="text-center">{urlInfo.name}</div>
+                <div className="text-center">{urlInfo.url}</div>
+                <div className="text-center">
+                  <span
+                    className={`inline-flex justify-center px-2.5 py-0.5 rounded-full text-xs font-medium ${
+                      urlInfo.status === "정상"
+                        ? "bg-lime-100 text-slate-900"
+                        : urlInfo.status === "변경감지"
+                          ? "bg-blue-100 text-slate-900"
+                          : "bg-red-100 text-slate-900"
+                    }`}
+                  >
+                    {urlInfo.status}
+                  </span>
+                </div>
+                <div className="text-center">{urlInfo.changeCount}</div>
+                <div className="text-center">
+                  {formatTime(urlInfo.lastChecked).slice(0, -3)}
+                </div>
+                <div className="text-center">
+                  {formatInterval(urlInfo.checkInterval)}
+                </div>
+              </div>
+            ))}
+          </>
+        )}
       </div>
       {isModalOpen && <AddUrlModal onClose={() => setIsModalOpen(false)} />}
     </div>
