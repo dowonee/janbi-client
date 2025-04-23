6b2c595cee6dc2176cae7e7fdb1e3d80a887097f
dowonee <jjddwwyt@gmail.com>
2025-04-23 17:53:44 +0900

    feat: 인트로 페이지 구성 (#10)

---
+++ src/pages/Intro.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import LandingHeader from "../components/intro/LandingHeader";
+import ActionButtons from "../components/intro/ActionButtons";
+import FeatureSection from "../components/intro/FeatureSection";
+
+export default function IntroPage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-white">
+      <main>
+        <LandingHeader />
+        <ActionButtons />
+        <FeatureSection />
+      </main>
+    </div>
+  );
+}
