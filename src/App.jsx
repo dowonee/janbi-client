6cb1e8cfa49ca7dbe8f73bcf6bf7f8df62c06189
dowonee <jjddwwyt@gmail.com>
2025-04-23 17:23:54 +0900

    feat: 라우터에 Intro 페이지 추가 및 사이드바 분리 (#10)

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
 import Sidebar from "./components/dashboard/Sidebar";
+import Dashboard from "./pages/Dashboard";
+import IntroPage from "./pages/Intro";
 import History from "./pages/History";
 
 export default function App() {
   return (
     <Router>
-      <div className="flex">
-        <Sidebar />
-        <main className="flex-1 min-h-screen bg-gray-50 p-6">
-          <Routes>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/history" element={<History />} />
-          </Routes>
-        </main>
-      </div>
+      <Routes>
+        <Route path="/" element={<IntroPage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <LayoutWithSidebar>
+              <Dashboard />
+            </LayoutWithSidebar>
+          }
+        />
+        <Route
+          path="/history"
+          element={
+            <LayoutWithSidebar>
+              <History />
+            </LayoutWithSidebar>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
