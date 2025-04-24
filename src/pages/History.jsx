479f8b624123b9e737f7825919ef87bd34f2d601
dowonee <jjddwwyt@gmail.com>
2025-04-24 09:52:42 +0900

    feat: History 페이지에서 URL별 변경 이력 조회 구현 (#11)

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,5 +1,26 @@
-import UrlHistoryList from "../components/history/UrlHistoryList";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { fetchUrlHistory } from "../api/urlApi";
+import UrlHistoryItem from "../components/history/UrlHistoryItem";
 
 export default function History() {
-  return <UrlHistoryList />;
+  const { id } = useParams();
+  const [urlHistory, setUrlHistory] = useState(null);
+
+  useEffect(() => {
+    const loadUrlHistory = async () => {
+      const responsedUrlHistory = await fetchUrlHistory(id);
+
+      setUrlHistory(responsedUrlHistory);
+    };
+
+    loadUrlHistory();
+  }, [id]);
+
+  return (
+    <div className="max-w-4xl mx-auto bg-white p-6 rounded-lg shadow-sm">
+      <h2 className="text-xl font-bold mb-4">변경 내역</h2>
+      <UrlHistoryItem urlInfo={urlHistory} />
+    </div>
+  );
 }
