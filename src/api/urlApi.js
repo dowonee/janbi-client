17e30c84dc0f4e386e3a7b4018fe6d9c05dbbfec
dowonee <jjddwwyt@gmail.com>
2025-04-24 09:55:03 +0900

    feat: URL 목록 조회, 변경 이력 조회 API 추가

--- src/api/urlApi.js
+++ src/api/urlApi.js
@@ -1,10 +1,21 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export async function fetchUrls() {
+  const response = await axios.get(`${BASE_URL}/urls`);
+
+  return response.data;
+}
+
+export async function fetchUrlHistory(id) {
+  const response = await axios.get(`${BASE_URL}/urls/${id}/history`);
+
+  return response.data;
+}
+
 export async function createUrl(data) {
-  const response = await axios.post(
-    `${import.meta.env.VITE_API_BASE_URL}/urls`,
-    data,
-  );
+  const response = await axios.post(`${BASE_URL}/urls`, data);
 
   return response.data;
 }
