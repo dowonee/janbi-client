c1f5c6c61f8cfd20e4f0674716f30f3958d096f9
dowonee <jjddwwyt@gmail.com>
2025-08-03 16:57:31 +0900

    refactor: useModalStore를 타입스크립트로 마이그레이션

--- src/stores/useModalStore.ts
+++ src/stores/useModalStore.ts
@@ -1,6 +1,12 @@
 import { create } from "zustand";
 
-const useModalStore = create((set) => ({
+interface ModalState {
+  isModalOpen: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+}
+
+const useModalStore = create<ModalState>((set) => ({
   isModalOpen: false,
   openModal: () => set({ isModalOpen: true }),
   closeModal: () => set({ isModalOpen: false }),
